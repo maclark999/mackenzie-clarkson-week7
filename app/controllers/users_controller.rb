d8228559c0212f8085bafe34aede10360f18636f
mackenzie clarkson <maclark99@gmail.com>
2015-02-23 10:41:23 -0700

    user can create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,15 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else render 'new'
+    end
   end
 
   def show
